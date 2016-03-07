510cd98751272f62945a59ca617034be8e1f505e
MiniWitch3 <med94man@yahoo.com>
2016-03-06 19:58:54 -0800

    Pushing Stuff from today

---
+++ Assets/Scripts/Acts.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Acts : MonoBehaviour {
+
+	public void onClick(){
+
+		//move to the multiple scenes
+		Debug.Log("Let's begin.  Moving to the Scenes.");
+		SceneManager.LoadScene("Acts");
+	}
+}
