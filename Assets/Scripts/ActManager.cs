e87b679b754a26daf59359bb7ecb8b1d99ac5979
MiniWitch3 <med94man@yahoo.com>
2016-03-13 20:21:30 -0700

    More UI stuff

--- Assets/Scripts/ActManager.cs
+++ Assets/Scripts/ActManager.cs
@@ -6,35 +6,74 @@ using UnityEngine.UI;
 public class ActManager : MonoBehaviour {
 
 	public Button back;
-	public int act = 0;
-	public Button buttonname;
+	public static int act = 0;
+	public Button button1;
+	public Button button2;
+	public Button button3;
+	public static int x = 0;
+	public static int y = 0;
+	public static int z = 0;
 
-	public void onClick(){
+
+	void start(){
+		act = PlayerPrefs.GetInt ("scene");
+		x = PlayerPrefs.GetInt ("spdate");
+		y = PlayerPrefs.GetInt ("rob");
+		z = PlayerPrefs.GetInt ("fun");
+		if (act >= 1) {
+			back.interactable = false;
+		} else if (act >= 3) {
+			back.interactable = false;
+			Debug.Log ("Thanks for playing.");
+			Application.Quit ();
+		}
+		if (x >= 1) {
+			button1.interactable = false;
+		}
+		if (y >= 1) {
+			button2.interactable = false;
+		}
+		if (z >= 1) {
+			button3.interactable = false;
+		}
+	}
+
+
+	public void onClick(RectTransform leftclick){
 
 		back.interactable = false;
 		act++;
-		/*
+		Debug.Log ("Act " + act);
+
+		PlayerPrefs.SetInt ("scene", act);
+
+		switch (leftclick.name){
+
 		//choose an act
-		if (buttonname.GetComponentInChildren<Text>().text = "Speed Date") {
+		case "Speed Date":
 			Debug.Log ("Moving to Speed Date.");
-			SceneManager.LoadScene ("prototype");
-			buttonname.interactable = false;
+			button1.interactable = false;
+			PlayerPrefs.SetInt ("spdate", 1);
+			x = 1;
+			SceneManager.LoadScene ("Scene UI");
+			break;
 
-		} else if (buttonname.GetComponentInChildren<Text>().text = "Robbery") {
+		case "Robbery":
 			Debug.Log ("Moving to Robbery.");
-			SceneManager.LoadScene ("prototype");
-			buttonname.interactable = false;
+			button2.interactable = false;
+			PlayerPrefs.SetInt ("rob", 1);
+			y = 1;
+			SceneManager.LoadScene ("Scene UI");
+			break;
 
-		} else if (buttonname.GetComponentInChildren<Text>().text = "Funeral") {
+		case "Funeral":
 			Debug.Log ("Moving to Funeral.");
-			SceneManager.LoadScene ("prototype");
-			buttonname.interactable = false;
-		
-		}*/
+			button3.interactable = false;
+			PlayerPrefs.SetInt ("fun", 1);
+			z = 1;
+			SceneManager.LoadScene ("Scene UI");
+			break;
 
-		//temp code until transitions made proper
-		Debug.Log ("Moving to the next scene.");
-		SceneManager.LoadScene ("prototype");
-		buttonname.interactable = false;
+		}
 	}
 }
---
+++ Assets/Scripts/GetResponse.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GetResponse : MonoBehaviour {
+
+	public static int awkpts = 0;
+	public static int forpts = 0;
+	public static int witpts = 0;
+
+	void start(){
+
+		awkpts = PlayerPrefs.GetInt ("awkward");
+		forpts = PlayerPrefs.GetInt ("formal");
+		witpts = PlayerPrefs.GetInt ("witty");
+	}
+
+	public void onClick(RectTransform text){
+
+		Debug.Log ("You clicked a button.");
+
+		Debug.Log ("You made the " + text.name + " choice.");
+
+		switch(text.name){
+
+		case "Witty":
+			witpts += 5;
+			forpts -= 3;
+			awkpts -= 2;
+			Debug.Log (witpts);
+			break;
+		
+		case "Formal":
+			forpts += 5;
+			awkpts -= 3;
+			witpts -= 2;
+			Debug.Log (forpts);
+			break;
+
+		case "Awkward":
+			awkpts += 5;
+			witpts -= 3;
+			forpts -= 2;
+			Debug.Log (awkpts);
+			break;
+		}
+
+		PlayerPrefs.SetInt ("awkward", awkpts);
+		PlayerPrefs.SetInt ("formal", forpts);
+		PlayerPrefs.SetInt ("witty", witpts);
+	}
+}
