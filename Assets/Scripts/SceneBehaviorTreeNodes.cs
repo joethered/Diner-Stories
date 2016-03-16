0ec0e724fab2a9967c364343e8eb48707fb62fa9
Joe Rossi <joethered@gmail.com>
2016-03-15 20:13:40 -0700

    Added behavior tree for setting up the Speed Dating Scene.  Methods not implemented.

--- Assets/Scripts/SceneBehaviorTreeNodes.cs
+++ Assets/Scripts/SceneBehaviorTreeNodes.cs
@@ -7,7 +7,7 @@ using System;
 
 public abstract class Node
 {
-    public virtual bool excecute(State state)
+    public virtual bool execute(State state)
     {
         throw new NotImplementedException();
     }
@@ -17,7 +17,7 @@ public abstract class Node
 public abstract class Composite : Node
 {
     string name;
-    protected List<Node> children = new List<Node>();
+    public List<Node> children = new List<Node>();
 
     public Composite(List<Node> childNodes, String name = null)
     {
@@ -42,11 +42,11 @@ public class Selector : Composite
     public Selector(List<Node> childNodes, String name) : base(childNodes, name) { }
     public Selector(String name) : base(name) { }
 
-    public bool execute(State state)
+    public override bool execute(State state)
     {
         foreach (Node child in children)
         {
-            bool success = child.excecute(state);
+            bool success = child.execute(state);
             if (success)
             {
                 return true;
@@ -63,11 +63,11 @@ public class Sequence : Composite
     public Sequence (List<Node> childNodes, String name) : base(childNodes, name) { }
     public Sequence(String name) : base(name) { }
 
-    public bool execute(State state)
+    public override bool execute(State state)
     {
         foreach (Node child in children)
         {
-            bool continue_execution = child.excecute(state);
+            bool continue_execution = child.execute(state);
             if (!continue_execution)
             {
                 return false;
@@ -91,7 +91,7 @@ public class Check : Node
         this.check_function = checkType.GetMethod(check_function);
     }
 
-    public bool execute(State state)
+    public override bool execute(State state)
     {
         object[] parameters = new object[1];
         parameters[0] = state;
@@ -112,7 +112,7 @@ public class Action : Node
         this.action_function = actionType.GetMethod(action_function);
     }
 
-    public bool execute(State state)
+    public override bool execute(State state)
     {
         object[] parameters = new object[1];
         parameters[0] = state;
--- Assets/Scripts/SceneInitializer.cs
+++ Assets/Scripts/SceneInitializer.cs
@@ -7,31 +7,269 @@ using System.Collections;
 /// </summary>
 public class SceneInitializer {
 
-    /*public Selector setupSpeedDateTree()
+    public Selector setupSpeedDateTree()
     {
         Selector root = new Selector("Speed Date Setup");
+        {
+            //Conclusion Plan
+            Sequence toConclusionPlan = new Sequence("To Conclusion Plan");
+            {
+                Check isConclusionAct = new Check("isThirdScene");
+                Selector conclusionPlan = new Selector("Conclusion Plan");
+                {
+                    Sequence robberAndWidow = new Sequence("The Robber and the Widow");
+                    {
+                        Check isRobberPlotThreadActive = new Check("isRobberPlotThreadActive");
+                        Check isWidowedPlotThreadActive = new Check("isWidowedPlotThreadActive");
+                        Action setPlayerAsWidow = new Action("setPlayerAsWidow");
+                        Action setDateAsRobber = new Action("setDateAsRobber");
+                    }
+                    Sequence robberOnADate = new Sequence("The Robber Goes on a Date");
+                    {
+                        Check isRobberPlotThreadActive = new Check("isRobberPlotThreadActive");
+                        Selector whoIsTheRobber = new Selector("Who will play the Robber");
+                        {
+                            Sequence robberWasPlayer = new Sequence("The robber was the player");
+                            {
+                                Check wasRobberPlayer = new Check("wasRobberPlayer");
+                                Action setDateAsRobber = new Action("setDateAsRobber");
+                                Action setPlayerAsExisting = new Action("setPlayerAsExisting");
+                                robberWasPlayer.children.Add(wasRobberPlayer);
+                                robberWasPlayer.children.Add(setDateAsRobber);
+                                robberWasPlayer.children.Add(setPlayerAsExisting);
+                            }
+                            Sequence robberWasNotPlayer = new Sequence("The robber was not the player");
+                            {
+                                Action setPlayerAsRobber = new Action("setPlayerAsRobber");
+                                Action setDateAsExisting = new Action("setDateAsExisting");
+                                robberWasNotPlayer.children.Add(setPlayerAsRobber);
+                                robberWasNotPlayer.children.Add(setDateAsExisting);
+                            }
+                            
+                            whoIsTheRobber.children.Add(robberWasPlayer);
+                            whoIsTheRobber.children.Add(robberWasNotPlayer);
+                        }
+                        robberOnADate.children.Add(isRobberPlotThreadActive);
+                        robberOnADate.children.Add(whoIsTheRobber);
+                    }
+                    Sequence widowOnADate = new Sequence("The Widow Goes on a Date");
+                    {
+                        Check wasWidowPlayer = new Check("wasWidowPlayer");
+                        Action setDateAsWidow = new Action("setDateAsWidow");
+                        Action setPlayerAsExisting = new Action("setPlayerAsExisting");
+                        widowOnADate.children.Add(wasWidowPlayer);
+                        widowOnADate.children.Add(setDateAsWidow);
+                        widowOnADate.children.Add(setPlayerAsExisting);
+                    }
+                    Sequence playerWidow = new Sequence("The Player is the Widow");
+                    {
+                        Action setPlayerAsWidow = new Action("setPlayerAsWidow");
+                        Action setDateAsExisting = new Action("setDateAsExisting");
+                        widowOnADate.children.Add(setPlayerAsWidow);
+                        widowOnADate.children.Add(setDateAsExisting);
+                    }
+                    conclusionPlan.children.Add(robberAndWidow);
+                    conclusionPlan.children.Add(robberOnADate);
+                    conclusionPlan.children.Add(widowOnADate);
+                    conclusionPlan.children.Add(playerWidow);
+                }
+                toConclusionPlan.children.Add(isConclusionAct);
+                toConclusionPlan.children.Add(conclusionPlan);
+            }
+            
 
+            //Climactic Plan
+            Sequence toClimacticPlan = new Sequence("To Climactic Plan");
+            {
+                Check isClimacticScene = new Check("isSecondScene");
+                Selector climaticPlan = new Selector("ClimacticPlan");
+                {
+                    Sequence datingWithRobber = new Sequence("Dater With Robber");
+                    {
+                        Check isRobberPlotThreadActive = new Check("isRobberPlotThreadActive");
+                        Selector whoIsTheRobber = new Selector("Who will play the Robber");
+                        {
+                            Sequence robberWasPlayer = new Sequence("The robber was the player");
+                            {
+                                Check wasRobberPlayer = new Check("wasRobberPlayer");
+                                Action robberDate = new Action("makeRobberDate");
+                                robberWasPlayer.children.Add(wasRobberPlayer);
+                                robberWasPlayer.children.Add(robberDate);
+                            }
 
-        Sequence conclusionPlan = new Sequence("Conclusion Plan");
-            Check isConclusionAct = new Check("isThirdScene");
-            Selector plotThreads = new Selector("Follow Plot Threads");
-                Sequence theLoss = new Sequence("The Loss");
-                    Check wasMurdered = new Check("wasMurdered");
-                    //Action
-                Sequence theRobber = new Sequence("The Robber");
-                    //Check
-                
-        Selector climaticPlan = new Selector("ClimaticPlan");
-            Sequence datingWithRobber = new Sequence("Dater With Robber");
-                Check 
-
-        Sequence expositionPlan = new Sequence("Exposition Plan");
-        Check isExpositionAct = new Check("isFirstScene");
-        Action createRandomSetup = new Action("createRandomSetup");
-        
-
-
-
+                            Action robberPlayer = new Action("makerRobberPlayer");
+                            whoIsTheRobber.children.Add(robberWasPlayer);
+                            whoIsTheRobber.children.Add(robberPlayer);
+                        }
+                        datingWithRobber.children.Add(isRobberPlotThreadActive);
+                        datingWithRobber.children.Add(whoIsTheRobber);
+                        
+                    }
+                    Sequence widowedMovesOn = new Sequence("Widowed Moves On");
+                    { 
+                        Check isWidowedPlotThreadActive = new Check("isWidowedPlotThreadActive");
+                        Selector whoIsWidow = new Selector("Who will play the Widow");
+                        {
+                            Sequence widowWasPlayer = new Sequence("The widow was the player");
+                            {
+                                Check wasWidowPlayer = new Check("wasWidowPlayer");
+                                Action widowDate = new Action("makeWidowDate");
+                                widowWasPlayer.children.Add(wasWidowPlayer);
+                                widowWasPlayer.children.Add(widowDate);
+                            }
+                            Action widowPlayer = new Action("widowPlayer");
+                            whoIsWidow.children.Add(widowPlayer);
+                        }
+                        
+                        widowedMovesOn.children.Add(isWidowedPlotThreadActive);
+                        widowedMovesOn.children.Add(whoIsWidow);
+                        
+                    }
+                    climaticPlan.children.Add(datingWithRobber);
+                    climaticPlan.children.Add(widowedMovesOn);
+                }
+                toClimacticPlan.children.Add(isClimacticScene);
+                toClimacticPlan.children.Add(climaticPlan);
+            }
+            //Exposition Plan
+            Action createRandomSetup = new Action("createRandomSetup");
+            root.children.Add(toConclusionPlan);
+            root.children.Add(toClimacticPlan);
+            root.children.Add(createRandomSetup);
+        }
         return root;
-    }*/
+    }
+
+    public Selector setupRobberyTree()
+    {
+        Selector root = new Selector("Robbery Setup");
+        {
+            //Conclusion Plan
+            Sequence toConclusionPlan = new Sequence("To Conclusion Plan");
+            {
+                Check isConclusionAct = new Check("isThirdScene");
+                Selector conclusionPlan = new Selector("Conclusion Plan");
+                {
+                    Sequence robberAndWidow = new Sequence("The Robber and the Widow");
+                    {
+                        Check isRobberPlotThreadActive = new Check("isRobberPlotThreadActive");
+                        Check isWidowedPlotThreadActive = new Check("isWidowedPlotThreadActive");
+                        Action setPlayerAsWidow = new Action("setPlayerAsWidow");
+                        Action setDateAsRobber = new Action("setDateAsRobber");
+                    }
+                    Sequence robberOnADate = new Sequence("The Robber Goes on a Date");
+                    {
+                        Check isRobberPlotThreadActive = new Check("isRobberPlotThreadActive");
+                        Selector whoIsTheRobber = new Selector("Who will play the Robber");
+                        {
+                            Sequence robberWasPlayer = new Sequence("The robber was the player");
+                            {
+                                Check wasRobberPlayer = new Check("wasRobberPlayer");
+                                Action setDateAsRobber = new Action("setDateAsRobber");
+                                Action setPlayerAsExisting = new Action("setPlayerAsExisting");
+                                robberWasPlayer.children.Add(wasRobberPlayer);
+                                robberWasPlayer.children.Add(setDateAsRobber);
+                                robberWasPlayer.children.Add(setPlayerAsExisting);
+                            }
+                            Sequence robberWasNotPlayer = new Sequence("The robber was not the player");
+                            {
+                                Action setPlayerAsRobber = new Action("setPlayerAsRobber");
+                                Action setDateAsExisting = new Action("setDateAsExisting");
+                                robberWasNotPlayer.children.Add(setPlayerAsRobber);
+                                robberWasNotPlayer.children.Add(setDateAsExisting);
+                            }
+
+                            whoIsTheRobber.children.Add(robberWasPlayer);
+                            whoIsTheRobber.children.Add(robberWasNotPlayer);
+                        }
+                        robberOnADate.children.Add(isRobberPlotThreadActive);
+                        robberOnADate.children.Add(whoIsTheRobber);
+                    }
+                    Sequence widowOnADate = new Sequence("The Widow Goes on a Date");
+                    {
+                        Check wasWidowPlayer = new Check("wasWidowPlayer");
+                        Action setDateAsWidow = new Action("setDateAsWidow");
+                        Action setPlayerAsExisting = new Action("setPlayerAsExisting");
+                        widowOnADate.children.Add(wasWidowPlayer);
+                        widowOnADate.children.Add(setDateAsWidow);
+                        widowOnADate.children.Add(setPlayerAsExisting);
+                    }
+                    Sequence playerWidow = new Sequence("The Player is the Widow");
+                    {
+                        Action setPlayerAsWidow = new Action("setPlayerAsWidow");
+                        Action setDateAsExisting = new Action("setDateAsExisting");
+                        widowOnADate.children.Add(setPlayerAsWidow);
+                        widowOnADate.children.Add(setDateAsExisting);
+                    }
+                    conclusionPlan.children.Add(robberAndWidow);
+                    conclusionPlan.children.Add(robberOnADate);
+                    conclusionPlan.children.Add(widowOnADate);
+                    conclusionPlan.children.Add(playerWidow);
+                }
+                toConclusionPlan.children.Add(isConclusionAct);
+                toConclusionPlan.children.Add(conclusionPlan);
+            }
+
+
+            //Climactic Plan
+            Sequence toClimacticPlan = new Sequence("To Climactic Plan");
+            {
+                Check isClimacticScene = new Check("isSecondScene");
+                Selector climaticPlan = new Selector("ClimacticPlan");
+                {
+                    Sequence datingWithRobber = new Sequence("Dater With Robber");
+                    {
+                        Check isRobberPlotThreadActive = new Check("isRobberPlotThreadActive");
+                        Selector whoIsTheRobber = new Selector("Who will play the Robber");
+                        {
+                            Sequence robberWasPlayer = new Sequence("The robber was the player");
+                            {
+                                Check wasRobberPlayer = new Check("wasRobberPlayer");
+                                Action robberDate = new Action("makeRobberDate");
+                                robberWasPlayer.children.Add(wasRobberPlayer);
+                                robberWasPlayer.children.Add(robberDate);
+                            }
+
+                            Action robberPlayer = new Action("makerRobberPlayer");
+                            whoIsTheRobber.children.Add(robberWasPlayer);
+                            whoIsTheRobber.children.Add(robberPlayer);
+                        }
+                        datingWithRobber.children.Add(isRobberPlotThreadActive);
+                        datingWithRobber.children.Add(whoIsTheRobber);
+
+                    }
+                    Sequence widowedMovesOn = new Sequence("Widowed Moves On");
+                    {
+                        Check isWidowedPlotThreadActive = new Check("isWidowedPlotThreadActive");
+                        Selector whoIsWidow = new Selector("Who will play the Widow");
+                        {
+                            Sequence widowWasPlayer = new Sequence("The widow was the player");
+                            {
+                                Check wasWidowPlayer = new Check("wasWidowPlayer");
+                                Action widowDate = new Action("makeWidowDate");
+                                widowWasPlayer.children.Add(wasWidowPlayer);
+                                widowWasPlayer.children.Add(widowDate);
+                            }
+                            Action widowPlayer = new Action("widowPlayer");
+                            whoIsWidow.children.Add(widowPlayer);
+                        }
+
+                        widowedMovesOn.children.Add(isWidowedPlotThreadActive);
+                        widowedMovesOn.children.Add(whoIsWidow);
+
+                    }
+                    climaticPlan.children.Add(datingWithRobber);
+                    climaticPlan.children.Add(widowedMovesOn);
+                }
+                toClimacticPlan.children.Add(isClimacticScene);
+                toClimacticPlan.children.Add(climaticPlan);
+            }
+            //Exposition Plan
+            Action createRandomSetup = new Action("createRandomSetup");
+            root.children.Add(toConclusionPlan);
+            root.children.Add(toClimacticPlan);
+            root.children.Add(createRandomSetup);
+        }
+        return root;
+    }
 }
